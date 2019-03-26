165bfa6dc2877017a68f08bb65e19707e72f9cb6
Will Engel <willengel4@gmail.com>
2019-03-26 16:39:42 -0400

    updates

--- data_loading.py
+++ data_loading.py
@@ -1,24 +1,19 @@
 import re
 
-class Team:
-    def __init__(self, id, name, city, state):
-        self.id = id
-        self.city = city
-        self.state = state
-        self.players = []
-
 class Player:
-    def __init__(self, id, name, height, weight, birthDate, college, team)
+    def __init__(self, id, number, firstName, lastName, height, weight, birthDate, college):
         self.id = id
-        self.name = name
+        self.number = number
+        self.firstName = firstName
+        self.lastName = lastName
         self.height = height
         self.weight = weight
         self.birthDate = birthDate
         self.college = college
-        self.team = team
 
 class Play:
-    def __init__()
+    def __init__(self):
+        self.abc = 1
 
 def loadFile(fileName, fileSep):
     return [line.split(fileSep) for line in open(fileName).readlines()]
@@ -36,6 +31,28 @@ def projection(columns, data, columnsToInclude):
     indices = [columns[column] for column in columnsToInclude]
     return columnIndices(columnsToInclude), [[row[index] for index in indices] for row in data]
 
+#id for team = city.year
+teams = {line[:-1] + "2018" : line[:-1] for line in open('2018/team_cities.txt').readlines()}
+
+players = dict()
+#id for player city.year.number.lastname
+for team_id in teams:
+    city = teams[team_id]
+    rawRosterData = loadFile('2018/' + city + ".csv", ',')
+    rosterColumns, rosterData = seperateHeadBody(rawRosterData)
+    for row in rosterData:
+        name = row[rosterColumns['Player']]
+        firstName = name.split('\\')[0].split(' ')[0]
+        lastName = name.split('\\')[0].split(' ')[1]
+        number = row[rosterColumns['No.']]
+        height = row[rosterColumns['Ht']]
+        weight = row[rosterColumns['Wt']]
+        birthDate = row[rosterColumns['BirthDate']]
+        college = row[rosterColumns['College/Univ']]
+        id = city + "2018" + number + lastName
+        players[id] = Player(id, number, firstName, lastName, height, weight, birthDate)
+
+'''
 #Loads raw play by play data
 raw_pbp_data = loadFile('2018/pbp-2018_tabs.txt', '\t')
 columns, data = seperateHeadBody(raw_pbp_data)
@@ -52,3 +69,4 @@ passes = selection(singleGameColumns, singleGame, 'PlayType', 'PASS')
 for row in passes:
     playDescription = row[singleGameColumns['Description']]
     playersInvolved = re.findall("\d*-\w\.[A-Z]\w*", playDescription)
+'''
